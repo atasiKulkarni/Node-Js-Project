a0eb8bb52999128378f762c1b7700ba801ff4fad
Atasi Kulkarni <75190043+atasikulkarni@users.noreply.github.com>
2021-04-23 17:39:00 +0530

    Add files via upload

--- Calculate Current Age/routes/currentAge.js
+++ Calculate Current Age/routes/currentAge.js
@@ -1,1 +1,25 @@
+/**
+ * Function to calculate area of circle
+ * and send back the response to client
+ * @param {*} name
+ *  @param {*} year
+ * @param {*} age
+ *  @param {*} res
+ */
+ function areaOfCircleRoute(name, year, res) {
+   
+    const studentName = name;
+    const  d = new Date(); 
+	const currentYear = d.getFullYear();
+    const age = Math.abs(currentYear - year);
 
+   
+    res.setHeader("Content-Type", "application/json");
+   
+    res.write("<p>hello " +name+ "</p>");
+    res.write('\n');
+    res.write("<p>You are currently " +age+ " years old</p>");
+    res.end();
+  }
+  
+  module.exports.areaOfCircleRoute = areaOfCircleRoute;
