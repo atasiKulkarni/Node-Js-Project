b8ded82533e3da4674f28dec8a4125ee7ad8ecff
Atasi Kulkarni <75190043+atasikulkarni@users.noreply.github.com>
2021-04-23 17:37:55 +0530

    Add files via upload

--- Calculate Current Age/index.js
+++ Calculate Current Age/index.js
@@ -1,1 +1,31 @@
+// Step 1: Import HTTP and URL module
+const http = require("http");
+const url = require("url");
+const areaOfCircleFile = require("./routes/currentAge");
 
+
+// create server function
+http
+  .createServer(function (req, res) {
+    // path variables
+    const path = req.url;
+
+    // query params from path
+    const queryParamsObject = url.parse(path, true).query;
+
+    // creating routes
+    if (path.includes("age")) {
+      const name = queryParamsObject.name;
+      const year = queryParamsObject.year;
+      const age = queryParamsObject.age;
+      areaOfCircleFile.areaOfCircleRoute(name, year, res, age);
+    
+    
+    } else {
+      res.setHeader("Content-Type", "text/html");
+      res.write("<h1>Hello World<h1>");
+      
+      res.end();
+    }
+  })
+  .listen(8080);
