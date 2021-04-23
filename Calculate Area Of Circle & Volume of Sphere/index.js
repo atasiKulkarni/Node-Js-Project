7328e75f7eb826da140d188ff332ca0849ade38a
Atasi Kulkarni <75190043+atasikulkarni@users.noreply.github.com>
2021-04-23 17:51:11 +0530

    Add files via upload

--- Calculate Area Of Circle & Volume of Sphere/index.js
+++ Calculate Area Of Circle & Volume of Sphere/index.js
@@ -1,1 +1,36 @@
+// Step 1: Import HTTP and URL module
+const http = require("http");
+const url = require("url");
+const areaOfCircleFile = require("./routes/areaOfCircle");
+const areaOfTriangleFile = require("./routes/volumeOfSphere");
 
+// create server function
+http
+  .createServer(function (req, res) {
+    // path variables
+    const path = req.url;
+
+    // query params from path
+    const queryParamsObject = url.parse(path, true).query;
+
+    // creating routes
+    if (path.includes("calculateAreaOfCircle")) 
+    {
+      const radius = queryParamsObject.radius;
+      
+      areaOfCircleFile.areaOfCircleRoute(radius, res);
+    } 
+    else if (path.includes("calculateVolumeOfSphere")) 
+    {
+        const radius = queryParamsObject.radius;
+         areaOfTriangleFile.volumeOfSphereRoute(radius, res);
+    } 
+     else 
+     {
+      res.setHeader("Content-Type", "text/html");
+      res.write("<h1>Hello World<h1>");
+      res.write("<h2>Site description</h2>");
+      res.end();
+    }
+  })
+  .listen(8080);
